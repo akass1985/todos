9dab38251b8fce342c331e2082309ad498ebc3c4
Алексей Кассь <akass1985@gmail.com>
2019-10-27 19:22:38 +0500

    Седал сохранение отредактированных задач

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,13 +1,18 @@
 const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'SAVE_TODO':
-            return [
-              ...state,
-              action.item
-            ]
-        default:
-          return state
-          }
-    }
+  switch (action.type) {
+     case 'SAVE_TODO':	
+       const existingTodoIdx = state.findIndex( t => t.id === action.item.id );
+       let newState;
+       if (existingTodoIdx > -1) {
+         newState = [...state];
+	 newState[existingTodoIdx] = action.item
+       } else {
+         newState = state.concat([action.item]);
+       }
+       return newState;
+     default:
+       return state
+  }
+}
 
 export default todos
